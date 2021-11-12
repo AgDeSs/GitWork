52b4add2f442f8759cc77ba0d08af7b739f67c57
kolankov <kolankov-goggle@mail.ru>
2021-11-12 22:06:47 +0300

    Add else

--- SecondCommit.py
+++ SecondCommit.py
@@ -1,6 +1,9 @@
 print("Hello, GitHub!")
 print(input("Your name: "))
 print(input("Your surname: "))
-age = print(input("Your age: "))
+age = input("Your age: ")
+print(age)
 if(age < "18"):
     print("Access is denied")
+else:
+    print("Access is allowed")
